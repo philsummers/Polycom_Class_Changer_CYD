b7465fa685cdb72befbc5d3a6d91a632ea4de586
Phil Summers <phil@blindlime.org>
2026-09-07 20:40:39 +0100

    Refactored polycom code into its own files

---
+++ include/polycom.h
@@ -0,0 +1,20 @@
+
+#include <Arduino.h>
+#include <SPIFFS.h>
+#include <FS.h>
+#include <WiFi.h>
+#include <esp_wifi.h>
+#include <WiFiUdp.h>
+
+#define POLYCOM_CHANNEL(x) (25+x)
+
+uint8_t pcmu_encode(int16_t pcm);
+void sendStartPackets(uint8_t channel);
+void sendEndPacket(uint8_t channel);
+void sendAudioPacket(uint8_t* prev, uint8_t* curr, bool first, uint8_t channel);
+bool readFrame(uint8_t* ulawBuf);
+void streamWav(uint8_t channel);
+void broadcastAudio(uint8_t channel);
+void populateSerial();
+void polycomSetup();
+
